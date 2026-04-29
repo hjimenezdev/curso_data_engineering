5f3604f1cef7a157f16b501f087bdeadb32a5754
Hugo Jimenez Muñoz <hjimenezdev@gmail.com>
2026-04-29 17:40:06 +0000

    Modelos incrementales y packages

--- models/staging/google_sheets/stg_google_sheets__budget.sql
+++ models/staging/google_sheets/stg_google_sheets__budget.sql
@@ -1,4 +1,6 @@
-{{ config(materialized='view') }}
+---------------------- Vista NO incremental ----------------------
+
+ {{ config(materialized='view') }}
 
 WITH src_budget AS (
     SELECT * FROM {{ source('google_sheets','budget') }}
@@ -14,4 +16,83 @@ renamed_casted AS (
     FROM src_budget
 )
 
-SELECT * FROM renamed_casted
+SELECT * FROM renamed_casted 
+
+---------------------- Estrategia MERGE - Transient table ----------------------
+/* 
+ {{ config(
+    materialized='incremental',
+    unique_key='_row',
+    incremental_strategy='merge'
+) }}
+
+WITH src_budget AS (
+    SELECT * FROM {{ source('google_sheets','budget') }}
+),
+
+renamed_casted AS (
+    SELECT
+        _row
+        , product_id
+        , quantity
+        , month
+        , _fivetran_synced AS date_load
+    FROM src_budget
+    {% if is_incremental() %}
+        WHERE date_load > (SELECT MAX(date_load) FROM {{ this }})
+    {% endif %}
+)
+
+SELECT * FROM renamed_casted */
+
+---------------------- Estrategia APPEND - Temporary table ----------------------
+/*
+ {{ config(
+    materialized='incremental',
+    incremental_strategy='append'
+) }}
+
+WITH src_budget AS (
+    SELECT * FROM {{ source('google_sheets','budget') }}
+),
+
+renamed_casted AS (
+    SELECT
+        _row
+        , product_id
+        , quantity
+        , month
+        , _fivetran_synced AS date_load
+    FROM src_budget
+    {% if is_incremental() %}
+        WHERE date_load > (SELECT MAX(date_load) FROM {{ this }})
+    {% endif %}
+)
+
+SELECT * FROM renamed_casted */
+
+---------------------- Estrategia DELETE+INSERT (por partición) - Temporary table ----------------------
+/* {{ config(
+    materialized='incremental',
+    unique_key='_row',
+    incremental_strategy='delete+insert'
+) }}
+
+WITH src_budget AS (
+    SELECT * FROM {{ source('google_sheets','budget') }}
+),
+
+renamed_casted AS (
+    SELECT
+        _row
+        , product_id
+        , quantity
+        , month
+        , _fivetran_synced AS date_load
+    FROM src_budget
+    {% if is_incremental() %}
+        WHERE date_load > (SELECT MAX(date_load) FROM {{ this }})
+    {% endif %}
+)
+
+SELECT * FROM renamed_casted */
